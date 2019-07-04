c186c087218b0cd618c083e275d6fd247343b414
EvertonSasuke <evertonsilva0014@gmail.com>
2019-07-04 09:30:09 -0300

    Pacote de Classe

--- Gato/src/parque/pichau/Client.java
+++ Gato/src/parque/pichau/Client.java
@@ -1,12 +1,124 @@
 package parque.pichau;
 
 public class Client {
-    String name;
-    String lastName;
-    String Cpf;
-    String Rg;
-    String about;
-    String email;
-    String password;
-    String confirmPassword;
+    private String name;
+    private String lastName;
+    private String Cpf;
+    private String Rg;
+    private String about;
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the Cpf
+     */
+    public String getCpf() {
+        return Cpf;
+    }
+
+    /**
+     * @param Cpf the Cpf to set
+     */
+    public void setCpf(String Cpf) {
+        this.Cpf = Cpf;
+    }
+
+    /**
+     * @return the Rg
+     */
+    public String getRg() {
+        return Rg;
+    }
+
+    /**
+     * @param Rg the Rg to set
+     */
+    public void setRg(String Rg) {
+        this.Rg = Rg;
+    }
+
+    /**
+     * @return the about
+     */
+    public String getAbout() {
+        return about;
+    }
+
+    /**
+     * @param about the about to set
+     */
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the confirmPassword
+     */
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    /**
+     * @param confirmPassword the confirmPassword to set
+     */
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
