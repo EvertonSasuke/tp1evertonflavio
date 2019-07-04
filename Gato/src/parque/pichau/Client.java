a34fa61c624f61998eb0af7b676a6371985fd285
EvertonSasuke <evertonsilva0014@gmail.com>
2019-07-04 09:21:26 -0300

    Pacote de Classe

---
+++ Gato/src/parque/pichau/Client.java
@@ -0,0 +1,12 @@
+package parque.pichau;
+
+public class Client {
+    String name;
+    String lastName;
+    String Cpf;
+    String Rg;
+    String about;
+    String email;
+    String password;
+    String confirmPassword;
+}
