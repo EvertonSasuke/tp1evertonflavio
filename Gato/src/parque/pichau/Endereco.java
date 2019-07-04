f3283528969dddcfaf48e6760a383357bffae95b
flavioban1 <flavioigorlemosdossantos@gmail.com>
2019-07-04 10:06:23 -0300

    Endereco Java

---
+++ Gato/src/parque/pichau/Endereco.java
@@ -0,0 +1,154 @@
+package parque.pichau;
+
+public class Endereco {
+    private String Empresa;
+    private String Telefone;
+    private String CEP;
+    private String Cidade;
+    private String Estado;
+    private String Rua;
+    private String Bairro;
+    private int Numero;
+    private String Complemento;
+    private String Pais;   
+
+    /**
+     * @return the Empresa
+     */
+    public String getEmpresa() {
+        return Empresa;
+    }
+
+    /**
+     * @param Empresa the Empresa to set
+     */
+    public void setEmpresa(String Empresa) {
+        this.Empresa = Empresa;
+    }
+
+    /**
+     * @return the Telefone
+     */
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    /**
+     * @param Telefone the Telefone to set
+     */
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    /**
+     * @return the CEP
+     */
+    public String getCEP() {
+        return CEP;
+    }
+
+    /**
+     * @param CEP the CEP to set
+     */
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    /**
+     * @return the Cidade
+     */
+    public String getCidade() {
+        return Cidade;
+    }
+
+    /**
+     * @param Cidade the Cidade to set
+     */
+    public void setCidade(String Cidade) {
+        this.Cidade = Cidade;
+    }
+
+    /**
+     * @return the Estado
+     */
+    public String getEstado() {
+        return Estado;
+    }
+
+    /**
+     * @param Estado the Estado to set
+     */
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+
+    /**
+     * @return the Rua
+     */
+    public String getRua() {
+        return Rua;
+    }
+
+    /**
+     * @param Rua the Rua to set
+     */
+    public void setRua(String Rua) {
+        this.Rua = Rua;
+    }
+
+    /**
+     * @return the Bairro
+     */
+    public String getBairro() {
+        return Bairro;
+    }
+
+    /**
+     * @param Bairro the Bairro to set
+     */
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    /**
+     * @return the Numero
+     */
+    public int getNumero() {
+        return Numero;
+    }
+
+    /**
+     * @param Numero the Numero to set
+     */
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    /**
+     * @return the Complemento
+     */
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    /**
+     * @param Complemento the Complemento to set
+     */
+    public void setComplemento(String Complemento) {
+        this.Complemento = Complemento;
+    }
+
+    /**
+     * @return the Pais
+     */
+    public String getPais() {
+        return Pais;
+    }
+
+    /**
+     * @param Pais the Pais to set
+     */
+    public void setPais(String Pais) {
+        this.Pais = Pais;
+    }
+}
